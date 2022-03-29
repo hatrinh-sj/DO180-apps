88f7e00b43306afbbcdb422b2679369bc0e3d733
Student User <student@workstation.lab.example.com>
2022-03-29 13:59:24 -0400

    Fix syntax

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "A change is coming!\n"
-print "The next change is here.\n"
+print "A change is coming!\n";
+print "The next change is here.\n";
 ?>
